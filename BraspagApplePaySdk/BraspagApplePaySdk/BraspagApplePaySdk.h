046bd93f9573863fc43f507301ea5ad567dd0e44
Jeferson F. Nazario <jefnazario@gmail.com>
2019-09-29 14:06:14 -0300

    Creation of the sdk project

---
+++ BraspagApplePaySdk/BraspagApplePaySdk/BraspagApplePaySdk.h
@@ -0,0 +1,19 @@
+//
+//  BraspagApplePaySdk.h
+//  BraspagApplePaySdk
+//
+//  Created by Jeferson Nazario on 28/09/19.
+//  Copyright © 2019 jnazario.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BraspagApplePaySdk.
+FOUNDATION_EXPORT double BraspagApplePaySdkVersionNumber;
+
+//! Project version string for BraspagApplePaySdk.
+FOUNDATION_EXPORT const unsigned char BraspagApplePaySdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BraspagApplePaySdk/PublicHeader.h>
+
+
